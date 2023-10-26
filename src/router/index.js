033c141fc50ab6e0a308b65d7eec7952e30d38fb
Anjana Narasinghe <nmasnanjana.123@gmail.com>
2023-10-26 21:18:56 +0530

    Setup the Vue Routers.

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LatestView from "../views/LatestView.vue"
+import TellYourStoriesView from "../views/TellYourStoriesView.vue"
+import CategoriesView from "../views/CategoriesView.vue"
+import CountriesView from "../views/CountriesView.vue"
+import NotFoundView from "../views/NotFoundView.vue"
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
   },
+  {
+    path: '/latest-view',
+    name: 'LatestView',
+    component: LatestView
+  },
+  {
+    path: '/tell-your-stories',
+    name: 'TellYourStories',
+    component: TellYourStoriesView
+  },
+  {
+    path: '/categories/:id',
+    name: 'Categories',
+    component: CategoriesView
+  },
+  {
+    path: '/countries/:id',
+    name: 'Countries',
+    component: CountriesView,
+    props: true
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFoundView,
+    props: true
+  }
 ]
 
 const router = createRouter({
