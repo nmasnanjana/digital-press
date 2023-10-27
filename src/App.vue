de4a466786d8dfd6ee47f388a5a0ebf346c9df47
Anjana Narasinghe <nmasnanjana.123@gmail.com>
2023-10-27 11:24:27 +0530

    Create Nav Bar

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,152 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link>
+    <div class="navbar">
+      <div class="navbar-brand"><router-link to="/"> <h2> DigitalPress </h2> </router-link></div>
+      <div class="nav-links">
+        <router-link to="/" class="menu">Home</router-link>
+        <router-link :to="{name: 'LatestView'}" class="menu">Latest</router-link>
+        <router-link :to="{name: 'TellYourStories'}" class="menu">Tell Your Stories</router-link>
+        <div class="dropdown">
+          <a>Categories</a>
+          <div class="dropdown-content">
+            <router-link v-for="category in categories" :key="category.id" :to="{name: 'Categories', params: {id: category.urlName}}"> {{ category.name }} </router-link>
+          </div>
+        </div>
+
+        <div class="dropdown">
+          <a>Countries</a>
+          <div class="dropdown-content">
+            <router-link v-for="country in countries" :key="country.id" :to="{name: 'Countries', params: {id: country.urlName}}"> {{ country.name }} </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </nav>
+
   <router-view/>
+
 </template>
 
 <style>
+body {
+  background-color: #f7f7f7;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  margin: 0;
+  padding: 0;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
+.navbar {
+  overflow: hidden;
   font-weight: bold;
-  color: #2c3e50;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 5px 225px 10px 215px;
 }
 
-nav a.router-link-exact-active {
-  color: #b94242;
+.navbar-brand {
+  font-size: 20px;
+  font-weight: bold;
+  padding: 10px 20px;
+  color: black;
 }
+
+.nav-links {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.navbar a {
+  font-size: 16px;
+  color: black;
+  text-align: center;
+  text-decoration: none;
+  padding: 14px 16px;
+}
+
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown {
+  font-size: 16px;
+  border: none;
+  outline: none;
+  color: black;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.dropdown:hover{
+  background-color: lightgray;
+}
+
+.menu:hover {
+  background-color: lightgray;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: white;
+  min-width: 160px;
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {
+  background-color: lightgray;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
 </style>
+
+<script>
+import CategoriesView from "@/views/CategoriesView.vue";
+
+export default {
+  name: "App",
+  computed: {
+    CategoriesView() {
+      return CategoriesView
+    }
+  },
+  data() {
+    return {
+      categories: [
+        { name: 'Sports', id: 1, urlName: 'sports' },
+        { name: 'War', id: 2, urlName: 'war' },
+        { name: 'Politics', id: 3, urlName: 'politics' },
+        { name: 'Health', id: 4, urlName: 'health' }
+      ],
+      countries: [
+        { name: 'USA', id: 1, urlName: 'usa' },
+        { name: 'UK', id: 2, urlName: 'uk' },
+        { name: 'China', id: 3, urlName: 'china' },
+        { name: 'India', id: 4, urlName: 'india' }
+      ]
+    }
+  }
+}
+</script>
